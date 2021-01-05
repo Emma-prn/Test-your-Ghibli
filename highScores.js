b9cfb58f125fbac9b5212ed2d893c775b944b3cb
Emma-prn <60692797+Emma-prn@users.noreply.github.com>
2021-01-05 19:24:04 +0100

    Delete highScores.js

--- highScores.js
+++
@@ -1,8 +0,0 @@
-const list = document.getElementById('list');
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-console.log(highScores);
-highScores.forEach(score => {
-    const item = document.createElement('li');
-    item.innerHTML = `${score.name} - ${score.score}`;
-    list.appendChild(item);
-});
